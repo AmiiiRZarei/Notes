4cfaede20cef3055cb363b69940d97a4b68502de
AmiiiRZarei <66995158+AmiiiRZarei@users.noreply.github.com>
2020-06-21 00:06:59 +0430

    add comment

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        
+        //AMIIIRI
 
 
 
